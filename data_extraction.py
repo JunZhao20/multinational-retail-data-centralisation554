f3be462c07513c69ad10490f0275883c161ff998
Jun Zhao <junzhao1111@gmail.com>
2024-01-16 12:39:24 +0000

    Add retrieve pdf data method in DataExtraction class

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,6 @@
 import yaml
 import pandas as pd
+import tabula as tab
 from sqlalchemy import create_engine, MetaData
 
 class DatabaseConnector:
@@ -41,9 +42,8 @@ class DatabaseConnector:
         
 
 class DataExtractor(DatabaseConnector):
-    def __init__(self, table_name):
+    def __init__(self):
         super().__init__()
-        self.table_name = table_name
         
     def list_db_tables(self):
         db_engine = super().init_db_engine(super().read_db_creds())
@@ -54,20 +54,28 @@ class DataExtractor(DatabaseConnector):
         for table_name in table_names:
             print(table_name)
     
-    def read_rds_table(self):
-        df = pd.read_sql_table(self.table_name, con=self.init_db_engine)
+    def read_rds_table(self, table_name):
+        df = pd.read_sql_table(table_name, con=self.init_db_engine)
         return df
+    
+    def retrieve_pdf_data(self, pdf_path):
+        df = tab.read_pdf(pdf_path, pages='all')
+        print(df)
         
         
         
 try:
     db_conn = DatabaseConnector()
     engine = db_conn.init_db_engine
-    extract = DataExtractor('legacy_users')
-    from data_cleaning import DataCleaning
-    data_cleaner = DataCleaning()
-    df = data_cleaner.clean_user_data()
-    upload = extract.upload_to_db(df, 'dim_users')
+    pdf_path = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+    dataExtract = DataExtractor()
+    df = dataExtract.retrieve_pdf_data(pdf_path)
+    print(df)
+    # extract = DataExtractor('legacy_users')
+    # from data_cleaning import DataCleaning
+    # data_cleaner = DataCleaning()
+    # df = data_cleaner.clean_user_data()
+    # upload = extract.upload_to_db(df, 'dim_users')
     # extract.read_rds_table()
     # extract.list_db_tables()
     
